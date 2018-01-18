e10bfa32b72774ccc363a88ae8c27b8a042dbb7b
Manuel J.González Rodríguez <majegonro@gmail.com>
2018-01-18 16:27:14 +0100

    Request login and pinpad stuff

--- ing-scraper/ing.py
+++ ing-scraper/ing.py
@@ -13,29 +13,97 @@
 
 #  You should have received a copy of the GNU General Public License 
 #  along with this program. If not, see <http://www.gnu.org/licenses/>.
+import json
+import requests
 
+ENDPOINT_BASE = "https://ing.ingdirect.es"
+ENDPOINTS = {
+	"LOGIN" : ENDPOINT_BASE + "/genoma_login/rest/session",
+	"POST_AUTH" : ENDPOINT_BASE + "/genoma_api/login/auth/response",
+	"CLIENT" : ENDPOINT_BASE + "/genoma_api/rest/client",
+	"PRODUCTS" : ENDPOINT_BASE + "/genoma_api/rest/products"
+}
+
+verbose = False
 
 def login():
 
     def set_login_data():
-        pass
+        print("DNI: ", end='')
+        dni = input()
+        print("Birthday (dd/mm/aaaa): ", end='')
+        birthday = input()
+        return [dni, birthday]
 
     def request_pinpad(login_data):
-        pass
+        
+        dni = login_data[0]
+        birthday = login_data[1]
 
-    def send_pinpad_positions(positions):
-        pass
+        url = ENDPOINTS["LOGIN"]
+
+        headers = {
+            'Content-Type': 'application/json'
+        }
+
+        payload = {
+            "device": "desktop",
+            "loginDocument": {
+                "document": dni,
+                "documentType": 0,
+            },
+            "birthday": birthday
+        }
+
+        response = requests.post(url, headers = headers, data = payload)
+        response = json.loads(response.text)
+        pinpad_data = {
+            "pinPadNumbers": response["pinPadNumbers"],
+            "pinPositions": response["pinPositions"],
+        }
+        return pinpad_data
+
+
+    def set_pinpad_positions(pinpad_data):
+        return "TODO"
+
+    def request_ticket(pinpad_positions):
+        return "TODO"
 
     def request_auth_cookies(ticket):
-        pass
+        return "TODO"
 
-    pass
+    log("Starting login process...")
+
+    log("Login Data")
+    login_data = set_login_data()
+    log("Requesting pinpad")
+    pinpad_data = request_pinpad(login_data)
+    pinpad_positions = set_pinpad_positions(pinpad_data)
+    log("Requesting ticket")
+    ticket = request_ticket(pinpad_positions)
+    log("Requesting auth cookies")
+    auth_cookies = request_auth_cookies(ticket)
+    log("Done")
+    return auth_cookies
 
 def get_client(auth_cookies):
-    pass
+    return "TODO"
 
 def get_products(auth_cookies):
-    pass
+    return "TODO"
 
 def get_movements(auth_cookies, product, from_date, to_date):
-    pass
+    return "TODO"
+
+
+# UTILS
+
+def log(message, end = '\n'):
+    if verbose:
+        print(message, end = end)
+
+
+# TEST
+
+login()
