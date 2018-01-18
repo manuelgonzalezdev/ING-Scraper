317f99b286edfdc486dfe10672ab47ef6b132113
Manuel J.González Rodríguez <majegonro@gmail.com>
2018-01-18 17:29:50 +0100

    Login finished

--- ing-scraper/ing.py
+++ ing-scraper/ing.py
@@ -24,7 +24,7 @@ ENDPOINTS = {
 	"PRODUCTS" : ENDPOINT_BASE + "/genoma_api/rest/products"
 }
 
-verbose = False
+verbose = True
 
 def login():
 
@@ -47,41 +47,106 @@ def login():
         }
 
         payload = {
-            "device": "desktop",
-            "loginDocument": {
-                "document": dni,
-                "documentType": 0,
-            },
-            "birthday": birthday
-        }
-
+	    	"device": "desktop",
+	    	"loginDocument": {
+	    		"document": dni,
+	    		"documentType": 0
+	    	},
+	    	"birthday": birthday
+	    }
+        payload = json.dumps(payload)
         response = requests.post(url, headers = headers, data = payload)
-        response = json.loads(response.text)
-        pinpad_data = {
-            "pinPadNumbers": response["pinPadNumbers"],
-            "pinPositions": response["pinPositions"],
-        }
-        return pinpad_data
+        response_json = json.loads(response.text)
 
+        if "errorCode" in response_json or "message" in response_json:
+            print(response_json["message"])
+            raise KeyError("API Error")
+
+
+        cookies = response.cookies.get_dict()
+        pinpad_data = {
+            "pinPadNumbers": response_json["pinPadNumbers"],
+            "pinPositions": response_json["pinPositions"],
+        }
+
+        genoma_cookie = {
+            "genoma-session-id": cookies["genoma-session-id"]
+        }
+
+        return pinpad_data, genoma_cookie
 
     def set_pinpad_positions(pinpad_data):
-        return "TODO"
+        numbers = pinpad_data["pinPadNumbers"]
+        positions = pinpad_data["pinPositions"]
 
-    def request_ticket(pinpad_positions):
-        return "TODO"
+        reversed_numbers = [0] * 10
+        for i, number in enumerate(numbers):
+            reversed_numbers[number] = i
+
+        visual_pinpad = ["[ ]" if (n in positions) else "[*]" for n in range(1,7)]
+        visual_pinpad = ''.join(visual_pinpad)
+        print("Enter positions...")
+        print(visual_pinpad)
+        pin = []
+        for position in positions:
+            print("Position " + str(position) + "  <-  ", end='')
+            pin.append(int(input()))
+        
+        pin_positions = [ reversed_numbers[n] for n in pin]
+        
+        return pin_positions
+
+    def request_ticket(pin_positions, cookie):
+        url = ENDPOINTS["LOGIN"]
+
+        headers = {
+		    'Accept' : 'application/json, text/javascript, */*; q=0.01',
+    	    'Content-Type': 'application/json; charset=utf-8'
+	    }
+
+        payload = {
+		    "pinPositions" : pin_positions
+	    }
+        payload = json.dumps(payload)
+        response = requests.put(url, headers = headers, data=payload, cookies = cookie)
+        response_json = json.loads(response.text)
+
+        if "errorCode" in response_json or "message" in response_json:
+            print(response_json["message"])
+            raise KeyError("API Error")
+
+        ticket = response_json["ticket"]
+        return ticket
 
     def request_auth_cookies(ticket):
-        return "TODO"
+        url = ENDPOINTS["POST_AUTH"]
+
+        headers = {
+		    'Accept' : 'application/json, text/javascript, */*; q=0.01',
+	        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+	    }
+
+        payload = "ticket=%s&device=desktop" % ticket
+        response = requests.post(url, headers = headers, data = payload)
+        
+        cookies = response.cookies.get_dict()
+        print(cookies)
+        auth_cookies = {
+            "Ucookie": cookies["Ucookie"],
+            "genoma-session-id": cookies["genoma-session-id"]
+        }
+        return auth_cookies
+
 
     log("Starting login process...")
 
     log("Login Data")
     login_data = set_login_data()
     log("Requesting pinpad")
-    pinpad_data = request_pinpad(login_data)
-    pinpad_positions = set_pinpad_positions(pinpad_data)
+    pinpad_data, genoma_cookie = request_pinpad(login_data)
+    pin_positions = set_pinpad_positions(pinpad_data)
     log("Requesting ticket")
-    ticket = request_ticket(pinpad_positions)
+    ticket = request_ticket(pin_positions, genoma_cookie)
     log("Requesting auth cookies")
     auth_cookies = request_auth_cookies(ticket)
     log("Done")
@@ -102,8 +167,3 @@ def get_movements(auth_cookies, product, from_date, to_date):
 def log(message, end = '\n'):
     if verbose:
         print(message, end = end)
-
-
-# TEST
-
-login()
