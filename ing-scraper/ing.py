c8abf9a34d001dd4aab4982bc6f7bf0e6bca3b52
Manuel J.González Rodríguez <majegonro@gmail.com>
2018-01-18 15:00:33 +0100

    Initial structure

---
+++ ing-scraper/ing.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+#  This file is part of the ING-Scraper project (https://github.com/z3nk0/ING-Scraper).
+#  Copyright (c) 2018 Manuel Jesús González Rodríguez.
+ 
+#  This program is free software: you can redistribute it and/or modify  
+#  it under the terms of the GNU General Public License as published by  
+#  the Free Software Foundation, version 3.
+
+#  This program is distributed in the hope that it will be useful, but 
+#  WITHOUT ANY WARRANTY; without even the implied warranty of 
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+#  General Public License for more details.
+
+#  You should have received a copy of the GNU General Public License 
+#  along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+
+def login():
+
+    def set_login_data():
+        pass
+
+    def request_pinpad(login_data):
+        pass
+
+    def send_pinpad_positions(positions):
+        pass
+
+    def request_auth_cookies(ticket):
+        pass
+
+    pass
+
+def get_client(auth_cookies):
+    pass
+
+def get_products(auth_cookies):
+    pass
+
+def get_movements(auth_cookies, product, from_date, to_date):
+    pass
