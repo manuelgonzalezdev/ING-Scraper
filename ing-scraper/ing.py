b2276921a78c39f39136294b58ceeef4fdf324d1
Manuel J. González <manuel.g@bubocar.com>
2019-05-09 12:11:14 +0200

    - .gitignore updated  - code reformatted via flake8

--- ing-scraper/ing.py
+++ ing-scraper/ing.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 #  This file is part of the ING-Scraper project (https://github.com/z3nk0/ING-Scraper).
 #  Copyright (c) 2018 Manuel Jesús González Rodríguez.
- 
-#  This program is free software: you can redistribute it and/or modify  
-#  it under the terms of the GNU General Public License as published by  
+
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, version 3.
 
-#  This program is distributed in the hope that it will be useful, but 
-#  WITHOUT ANY WARRANTY; without even the implied warranty of 
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+#  This program is distributed in the hope that it will be useful, but
+#  WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 #  General Public License for more details.
 
-#  You should have received a copy of the GNU General Public License 
+#  You should have received a copy of the GNU General Public License
 #  along with this program. If not, see <http://www.gnu.org/licenses/>.
 '''
 
@@ -28,20 +28,21 @@ import getpass
 
 ENDPOINT_BASE = "https://ing.ingdirect.es"
 ENDPOINTS = {
-	"LOGIN" : ENDPOINT_BASE + "/genoma_login/rest/session",
-	"POST_AUTH" : ENDPOINT_BASE + "/genoma_api/login/auth/response",
-	"CLIENT" : ENDPOINT_BASE + "/genoma_api/rest/client",
-	"PRODUCTS" : ENDPOINT_BASE + "/genoma_api/rest/products"
+    "LOGIN": ENDPOINT_BASE + "/genoma_login/rest/session",
+    "POST_AUTH": ENDPOINT_BASE + "/genoma_api/login/auth/response",
+    "CLIENT": ENDPOINT_BASE + "/genoma_api/rest/client",
+    "PRODUCTS": ENDPOINT_BASE + "/genoma_api/rest/products"
 }
 
 HEADERS = {
-    'Accept' : 'application/json, text/javascript, */*; q=0.01',
+    'Accept': 'application/json, text/javascript, */*; q=0.01',
     'Content-Type': 'application/json; charset=utf-8'
 }
 
 verbose = False
 
-def login_with_terminal(dni = None, birthday = None):
+
+def login_with_terminal(dni=None, birthday=None):
     log("Starting login process...")
 
     if dni is None or birthday is None:
@@ -55,18 +56,20 @@ def login_with_terminal(dni = None, birthday = None):
 
     return auth_cookies
 
+
 def login_auto(dni, birthday, password):
     numbers, positions, genoma_cookie = request_login_data(dni, birthday)
-    
+
     if type(password) is not str:
         password = str(password)
-    
+
     numbers = reverse_pinpad_numbers(numbers)
     positions = [numbers[int(password[i-1])] for i in positions]
-    positions = list(map(int,positions))
+    positions = list(map(int, positions))
     ticket = request_ticket(positions, genoma_cookie)
     auth_cookies = request_auth_cookies(ticket)
     return auth_cookies
+
 
 def request_login_data(dni, birthday):
     log("Starting login process...")
@@ -74,7 +77,8 @@ def request_login_data(dni, birthday):
     pinpad_data, genoma_cookie = request_pinpad(dni, birthday)
     numbers = pinpad_data["pinPadNumbers"]
     positions = pinpad_data["pinPositions"]
-    return numbers, positions, genoma_cookie    
+    return numbers, positions, genoma_cookie
+
 
 def send_login_data(pin_positions, genoma_cookie):
     ticket = request_ticket(pin_positions, genoma_cookie)
@@ -82,6 +86,7 @@ def send_login_data(pin_positions, genoma_cookie):
     log("Done")
 
     return auth_cookies
+
 
 def set_login_data():
     log("Login Data")
@@ -91,20 +96,21 @@ def set_login_data():
     birthday = input()
     return dni, birthday
 
+
 def request_pinpad(dni, birthday):
     log("Requesting pinpad")
     url = ENDPOINTS["LOGIN"]
     headers = HEADERS
     payload = {
-    	"device": "desktop",
-    	"loginDocument": {
-    		"document": dni,
-    		"documentType": 0
-    	},
-    	"birthday": birthday
+        "device": "desktop",
+        "loginDocument": {
+            "document": dni,
+            "documentType": 0
+        },
+        "birthday": birthday
     }
     payload = json.dumps(payload)
-    response = requests.post(url, headers = headers, data = payload)
+    response = requests.post(url, headers=headers, data=payload)
     response_json = json.loads(response.text)
     if "errorCode" in response_json or "message" in response_json:
         print(response_json["message"])
@@ -119,20 +125,22 @@ def request_pinpad(dni, birthday):
     }
     return pinpad_data, genoma_cookie
 
+
 def set_pinpad_positions(pinpad_data):
     numbers = pinpad_data["pinPadNumbers"]
     positions = pinpad_data["pinPositions"]
     reversed_numbers = reverse_pinpad_numbers(numbers)
-    visual_pinpad = ["[ ]" if (n in positions) else "[*]" for n in range(1,7)]
+    visual_pinpad = ["[ ]" if (n in positions) else "[*]" for n in range(1, 7)]
     visual_pinpad = ''.join(visual_pinpad)
     print("Enter positions...")
     print(visual_pinpad)
     pin = []
     for position in positions:
         pin.append(int(getpass.getpass("Position " + str(position) + "  <-  ")))
-    
-    pin_positions = [ reversed_numbers[n] for n in pin]
+
+    pin_positions = [reversed_numbers[n] for n in pin]
     return pin_positions
+
 
 def reverse_pinpad_numbers(pinpad_numbers):
     reversed_numbers = [0] * 10
@@ -147,10 +155,10 @@ def request_ticket(pin_positions, cookie):
     url = ENDPOINTS["LOGIN"]
     headers = HEADERS
     payload = {
-	    "pinPositions" : pin_positions
+        "pinPositions": pin_positions
     }
     payload = json.dumps(payload)
-    response = requests.put(url, headers = headers, data=payload, cookies = cookie)
+    response = requests.put(url, headers=headers, data=payload, cookies=cookie)
     response_json = json.loads(response.text)
     if "errorCode" in response_json or "message" in response_json:
         print(response_json["message"])
@@ -158,13 +166,14 @@ def request_ticket(pin_positions, cookie):
     ticket = response_json["ticket"]
     return ticket
 
+
 def request_auth_cookies(ticket):
     log("Requesting auth cookies")
     url = ENDPOINTS["POST_AUTH"]
     headers = HEADERS.copy()
     headers['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8'
     payload = "ticket=%s&device=desktop" % ticket
-    response = requests.post(url, headers = headers, data = payload)
+    response = requests.post(url, headers=headers, data=payload)
     cookies = response.cookies.get_dict()
     auth_cookies = {
         "Ucookie": cookies["Ucookie"],
@@ -172,23 +181,26 @@ def request_auth_cookies(ticket):
     }
     return auth_cookies
 
+
 def get_client(auth_cookies):
     log("Requesting client data")
     url = ENDPOINTS["CLIENT"]
     headers = HEADERS
     cookies = auth_cookies
-    response = requests.get(url, headers = headers, cookies = cookies)
+    response = requests.get(url, headers=headers, cookies=cookies)
     response_json = json.loads(response.text)
     return response_json
+
 
 def get_products(auth_cookies):
     log("Requesting products data")
     url = ENDPOINTS["PRODUCTS"]
     headers = HEADERS
     cookies = auth_cookies
-    response = requests.get(url, headers = headers, cookies = cookies)
+    response = requests.get(url, headers=headers, cookies=cookies)
     response_json = json.loads(response.text)
     return response_json
+
 
 def get_movements(auth_cookies, product, from_date, to_date):
     log("Requesting product movements")
@@ -196,13 +208,13 @@ def get_movements(auth_cookies, product, from_date, to_date):
     url = url + "/%s/movements?fromDate=%s&toDate=%s&limit=100&offset=0" % (product, from_date, to_date)
     headers = HEADERS
     cookies = auth_cookies
-    response = requests.get(url, headers = headers, cookies = cookies)
+    response = requests.get(url, headers=headers, cookies=cookies)
     response_json = json.loads(response.text)
     return response_json
 
 
 # UTILS
 
-def log(message, end = '\n'):
+def log(message, end='\n'):
     if verbose:
-        print(message, end = end)
+        print(message, end=end)
